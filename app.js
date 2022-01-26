dd9068a57ab47eeaa1c70c29c329012fd8255679
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-01-26 17:53:25 +0630

    adding some features before connection

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-let connectedPerrs = [];
+let connectedPeers = [];
 
-io.on('connection', (socket) => {
-    connectedPerrs.push(socket.id);
-    console.log(connectedPerrs);
+io.on("connection", (socket) => {
+  connectedPeers.push(socket.id);
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+  socket.on("pre-offer", (data) => {
+    console.log("pre-offer-came");
+    const { calleePersonalCode, callType } = data;
+    console.log(calleePersonalCode);
+    console.log(connectedPeers);
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === calleePersonalCode
+    );
 
-        const newConnectedPerrs = connectedPerrs.filter((peerSocketId) => {
-            return peerSocketId !== socket.io;
-        });
+    console.log(connectedPeer);
 
-        connectedPerrs = newConnectedPerrs
-    });
+    if (connectedPeer) {
+      const data = {
+        callerSocketId: socket.id,
+        callType,
+      };
+      io.to(calleePersonalCode).emit("pre-offer", data);
+    } else {
+      const data = {
+        preOfferAnswer: "CALLEE_NOT_FOUND",
+      };
+      io.to(socket.id).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("pre-offer-answer", (data) => {
+    const { callerSocketId } = data;
+
+    const connectedPeer = connectedPeers.find(
+      (peerSocketId) => peerSocketId === callerSocketId
+    );
+
+    if (connectedPeer) {
+      io.to(data.callerSocketId).emit("pre-offer-answer", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+
+    const newConnectedPeers = connectedPeers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    );
+
+    connectedPeers = newConnectedPeers;
+    console.log(connectedPeers);
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-})
+  console.log(`listening on ${PORT}`);
+});
